7c9f02bb9e66346f961ccc61dedd77771121f6b4
bwax <braxton.waxdeck@gmail.com>
2022-10-19 20:33:10 -0700

    added html and js vars

---
+++ Assets/script.js
@@ -0,0 +1,28 @@
+var startButtonEl = document.querySelector
+("#start-quiz-button");
+var topBarInfoEl = document.querySelector
+("#top-bar-info");
+var quizIntroScreenEl = document.querySelector
+("#quiz-intro-screen");
+var questionScreenEl = document.querySelector
+("#question-screen");
+var questionTitleEl = document.querySelector
+("#question-title");
+var questionResponseEl = document.querySelector
+("#question-response");
+var quizFinishedScreenEl = document.querySelector
+("#quiz-finished-screen");
+var quizFinishedTitleEl = document.querySelector
+("#quiz-finished-title");
+var quizScoreEl = document.querySelector
+("#quiz-score");
+var highscoreScreenEl = document.querySelector
+("#highscore-screen");
+var highscoreTitleEl = document.querySelector
+("#highscore-title");
+var goBackButtonEl = document.querySelector
+("#go-back-button");
+var clearScoreButtonEl = document.querySelector
+("#clear-score-button");
+
+
